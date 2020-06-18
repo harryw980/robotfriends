b06234b5dcabfb6e557214bab340ab470ee81921
Hanlei Wang <hanlei430@gmail.com>
2020-06-18 19:02:30 -0400

    Updates

---
+++ precache-manifest.d1b80dc7276650cdf1914f7418fa07cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dadd7510935106a6b210308dc450e5a",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "10b2a9f5e1df9077f163",
+    "url": "/robotfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "37444409db73d4c053a6",
+    "url": "/robotfriends/static/css/main.8d938dcb.chunk.css"
+  },
+  {
+    "revision": "10b2a9f5e1df9077f163",
+    "url": "/robotfriends/static/js/2.9f361c63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotfriends/static/js/2.9f361c63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37444409db73d4c053a6",
+    "url": "/robotfriends/static/js/main.f54521a3.chunk.js"
+  },
+  {
+    "revision": "a31b76b7195354753f47",
+    "url": "/robotfriends/static/js/runtime-main.af337cd2.js"
+  },
+  {
+    "revision": "92c3810f48104a0fa26cbef3ad302807",
+    "url": "/robotfriends/static/media/electrical.92c3810f.woff"
+  }
+]);
