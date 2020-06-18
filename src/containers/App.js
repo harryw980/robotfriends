ac33322117565e639700642cb30db669af0eec69
Hanlei Wang <hanlei430@gmail.com>
2020-06-18 19:31:01 -0400

    fixed picture size

--- src/containers/App.js
+++ src/containers/App.js
@@ -35,7 +35,7 @@ class App extends React.Component {
 
         return(
             <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
+                <h1 style={{ fontSize: '200%' }}>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <Scroll>
                     <ErrorBoundary>
